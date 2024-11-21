15efc6cdfd98403d7cc0888a1ba431b000400f92
Lanzoness <mlbblance@gmail.com>
2024-11-21 10:38:27 +0800

    Created new screens; added new screens to App.tsx navigation container

--- App.tsx
+++ App.tsx
@@ -4,6 +4,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoadingScreen from './src/screens/LoadingScreen';
 import LoginUserAdmin from './src/screens/LoginUserAdmin';
 import LoginAuth from './src/screens/LoginAuth';
+import UserHomeScreen from './src/screens/UserHomeScreen';
+import SubmitLost from './src/screens/SubmitLost';
+import ViewLost from './src/screens/ViewLost';
 
 const Stack = createNativeStackNavigator();
 
@@ -24,6 +27,21 @@ function App(): React.JSX.Element {
         <Stack.Screen 
           name="LoginAuth" 
           component={LoginAuth}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+          name="UserHomeScreen" 
+          component={UserHomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+          name="SubmitLost" 
+          component={SubmitLost}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+          name="ViewLost" 
+          component={ViewLost}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>

04f204014646331eb466c2cb50eb2d4f34451ce8
Margs-reese <CMR1030@dlsud.edu.ph>
2024-11-24 12:18:23 +0800

    Added constants folder containing default font sizes, fonts and User-side color palette.
    NOTE: Paytone One and Ubuntu sans are yet to be
     added.

---
+++ src/constants/FontSize.tsx
@@ -0,0 +1,8 @@
+const FontSize = {
+    body_small: 14,
+    body_medium: 16,
+    sub_heading: 28,
+    header_font_size: 47,
+  };
+
+export default FontSize;
---
+++ src/constants/UserPalette.tsx
@@ -0,0 +1,11 @@
+// ./src/constants/UserPalette.tsx
+const UserPalette = {
+  default_background: '#FFFFFF',
+  green: '#00722A',
+  blue: 'rgba(10, 48, 105, 0.4)', // 40% opacity
+  text_field_bg: 'rgba(255, 255, 255, 0.3)',
+  white_font: '#FFFFFF',
+  active_button: 'rgba(10, 48, 105, 0.4)',
+};
+
+export default UserPalette;

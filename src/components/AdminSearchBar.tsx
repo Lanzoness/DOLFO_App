5448d343f0f896a51863fc2a9be5c09db0aff800
Margs-reese <CMR1030@dlsud.edu.ph>
2024-11-30 17:05:39 +0800

    updated checkbox and cancel button color

--- src/components/AdminSearchBar.tsx
+++ src/components/AdminSearchBar.tsx
@@ -105,7 +105,7 @@ const styles = StyleSheet.create({
     color: UserPalette.red_button,
   },
   okButton: {
-    backgroundColor: UserPalette.blue,
+    backgroundColor: UserPalette.secondary_blue,
     paddingVertical: 8,
     paddingHorizontal: 20,
     borderRadius: 5,

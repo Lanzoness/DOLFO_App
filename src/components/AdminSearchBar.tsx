1b8caf995dd4600532479f6219416dbc8410b820
Margs-reese <CMR1030@dlsud.edu.ph>
2024-11-30 12:57:09 +0800

    recolored the modal overlay to blue

--- src/components/AdminSearchBar.tsx
+++ src/components/AdminSearchBar.tsx
@@ -105,7 +105,7 @@ const styles = StyleSheet.create({
     color: UserPalette.red_button,
   },
   okButton: {
-    backgroundColor: UserPalette.green,
+    backgroundColor: UserPalette.blue,
     paddingVertical: 8,
     paddingHorizontal: 20,
     borderRadius: 5,
